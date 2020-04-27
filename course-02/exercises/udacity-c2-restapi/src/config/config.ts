5da4d8f3ea0192300a577f9a97906a0b222127d8
josep.burga <josep.burgaya@est.fib.upc.edu>
2020-04-27 18:17:41 +0200

    hotfix env variable

--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -5,20 +5,20 @@ export const config = {
     "database": process.env.UDAGRAM_DB_NAME,
     "host": process.env.UDAGRAM_DB_HOST,
     "dialect": "postgres",
-    "aws_region": "us-east-1",
-    "aws_profile": "default",
+    "aws_region": process.env.AWS_REGION,
+    "aws_profile": process.env.PROFILE,
     "aws_media_bucket": process.env.UDAGRAM_MEDIA_BUCKET,
     "jwt_secret":process.env.UDAGRAM_JWT_SECRET
   },
   "prod": {
-    "username": process.env.UDAGRAM_DB_USERNAME,
-    "password": process.env.UDAGRAM_DB_PASSWORD,
-    "database": process.env.UDAGRAM_DB_NAME,
-    "host": process.env.UDAGRAM_DB_HOST,
-    "dialect": "postgres",
-    "aws_region": "us-east-1",
-    "aws_profile": "default",
-    "aws_media_bucket": process.env.UDAGRAM_MEDIA_BUCKET,
-    "jwt_secret":process.env.UDAGRAM_JWT_SECRET
+      "username": process.env.UDAGRAM_DB_USERNAME,
+      "password": process.env.UDAGRAM_DB_PASSWORD,
+      "database": process.env.UDAGRAM_DB_NAME,
+      "host": process.env.UDAGRAM_DB_HOST,
+      "dialect": "postgres",
+      "aws_region": process.env.AWS_REGION,
+      "aws_profile": process.env.PROFILE,
+      "aws_media_bucket": process.env.UDAGRAM_MEDIA_BUCKET,
+      "jwt_secret":process.env.UDAGRAM_JWT_SECRET
   }
 }
