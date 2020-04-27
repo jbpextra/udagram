2de0ac60d527b90c90b9acc6f7bbe1dbdffc5f52
josep.burga <josep.burgaya@est.fib.upc.edu>
2020-04-27 14:32:25 +0200

    add endpoints

--- course-02/exercises/udacity-c2-restapi/src/controllers/v0/feed/routes/feed.router.ts
+++ course-02/exercises/udacity-c2-restapi/src/controllers/v0/feed/routes/feed.router.ts
@@ -16,14 +16,26 @@ router.get('/', async (req: Request, res: Response) => {
     res.send(items);
 });
 
-//@TODO
 //Add an endpoint to GET a specific resource by Primary Key
+router.get('/:id', async (req: Request, res: Response) => {
+    let { id } = req.params;
+    const item = await FeedItem.findByPk(id);
+    if (!item) {
+        return res.status(400).send({ message: 'Item id is required' });
+    }
+    res.send(item);
+});
 
 // update a specific resource
 router.patch('/:id', 
     requireAuth, 
     async (req: Request, res: Response) => {
-        //@TODO try it yourself
+        let { id } = req.params;
+        const item = await FeedItem.findByPk(id);
+        if (!item) {
+            return res.status(400).send({ message: 'Item id is required' });
+        }
+        const saved_item = item.save();
         res.send(500).send("not implemented")
 });
 
